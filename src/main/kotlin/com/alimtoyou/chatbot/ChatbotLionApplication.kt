bad595884f7b27ebbd4cbccdb7f21c902fecdbaa
sceonn <yoou201@naver.com>
2023-03-15 22:10:53 +0900

    init

---
+++ src/main/kotlin/com/alimtoyou/chatbot/ChatbotLionApplication.kt
@@ -0,0 +1,11 @@
+package com.alimtoyou.chatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatbotLionApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatbotLionApplication>(*args)
+}
---
+++ src/test/kotlin/com/alimtoyou/chatbot/ChatbotLionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alimtoyou.chatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatbotLionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
